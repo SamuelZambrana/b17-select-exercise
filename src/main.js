ee5e1e66ffd6a87f0474df24af17c86ef021d167
Marina <marina_esra@hotmail.com>
2025-03-18 19:48:39 +0100

    create select

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
 import "./scss/style.scss";
 
 console.log("test");
+
+/**
+ <select name="select">
+   <option value="value1">Value 1</option>
+   <option value="value2" selected>Value 2</option>
+   <option value="value3">Value 3</option>
+ </select>
+ */
+
+function colorSelect() {
+    const appElement = document.querySelector('#app');
+    const selectElement = document.createElement('select');
+    selectElement.setAttribute('name', 'select');
+    
+    ['red', 'blue', 'green'].forEach(color =>{
+        const optionColorElement = document.createElement('option')
+    })
+}
