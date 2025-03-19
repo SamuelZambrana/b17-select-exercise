f70272435c2622a8848f040d9c6f9a9bfd3efbc7
elena <elenaobregonl@gmail.com>
2025-03-19 18:14:50 +0100

    create box

--- src/main.js
+++ src/main.js
@@ -1,28 +1,27 @@
 import "./scss/style.scss";
 
-console.log("test");
-
-/*
- <select name="select">
-   <option value="value1">Value 1</option>
-   <option value="value2" selected>Value 2</option>
-   <option value="value3">Value 3</option>
- </select>
- */
-
 function colorSelect() {
-    const appElement = document.querySelector('#app');
-    const selectElement = document.createElement('select');
-    selectElement.setAttribute('name', 'select');
-    
-    ['Red', 'Blue', 'Green'].forEach(color =>{
-        const optionColorElement = document.createElement('option')
-        optionColorElement.setAttribute('value', color.toLowerCase())
-        optionColorElement.textContent = color
-        selectElement.appendChild(optionColorElement)
-    })
+  const appElement = document.querySelector("#app");
+  const selectElement = document.createElement("select");
+  selectElement.setAttribute("name", "select");
 
-    appElement.appendChild(selectElement)
+  ["Red", "Blue", "Green"].forEach((color) => {
+    const optionColorElement = document.createElement("option");
+    optionColorElement.setAttribute("value", color.toLowerCase());
+    optionColorElement.textContent = color;
+    selectElement.appendChild(optionColorElement);
+  });
+
+  appElement.appendChild(selectElement);
 }
 
-colorSelect()
+colorSelect();
+
+function createBox() {
+  const boxElement = document.createElement("div");
+  boxElement.classList = "box bg-red";
+  const appElement = document.querySelector("#app");
+  appElement.appendChild(boxElement);
+}
+
+createBox();
